586127adcb6280c04022640c8a5795975adaa79b
sadam <sami2010mohamed@gmail.com>
2024-06-10 14:35:18 +0300

    New Features

--- src/main/java/com/fithub/service/base/EmailService.java
+++ src/main/java/com/fithub/service/base/EmailService.java
@@ -15,4 +15,26 @@ public class EmailService {
         this.emailRepository = emailRepository;
         this.mapper = new ModelMapper();
     }
+
+
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void sendEmail(String to, String subject, String htmlBody, MultipartFile[] attachments) throws MessagingException, IOException {
+//        MimeMessage message = mailSender.createMimeMessage();
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//
+//        helper.setTo(to);
+//        helper.setSubject(subject);
+//        helper.setText(htmlBody, true);
+//
+//        if (attachments != null) {
+//            for (MultipartFile attachment : attachments) {
+//                InputStreamSource source = new ByteArrayResource(attachment.getBytes());
+//                helper.addAttachment(attachment.getOriginalFilename(), source);
+//            }
+//        }
+//
+//        mailSender.send(message);
+//    }
 }
