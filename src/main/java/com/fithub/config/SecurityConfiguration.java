dca28930d6636626709b966f3b448a66ae02bd1c
sadam <sami2010mohamed@gmail.com>
2024-08-19 14:44:15 +0300

    Files security and attachment controller

--- src/main/java/com/fithub/config/SecurityConfiguration.java
+++ src/main/java/com/fithub/config/SecurityConfiguration.java
@@ -31,6 +31,7 @@ public class SecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(("/api/v1/files/**")).permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/api/v1/user/**").hasAnyAuthority(Role.USER.name())
                         .anyRequest().authenticated()
--- src/main/java/com/fithub/controller/base/AttachmentController.java
+++ src/main/java/com/fithub/controller/base/AttachmentController.java
@@ -1,7 +1,7 @@
 package com.fithub.controller.base;
 
 import com.fithub.dto.base.AttachmentDTO;
-import com.fithub.model.base.ResponseModel;
+import com.fithub.exception.ResourceNotFoundException;
 import com.fithub.service.base.AttachmentService;
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.io.Resource;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
 
@@ -54,7 +52,7 @@ public class AttachmentController {
     }
 
     @GetMapping("/files/{fileName:.+}")
-    public ResponseModel<Base64> downloadFile(@PathVariable String fileName) {
+    public byte[] downloadFile(@PathVariable String fileName) {
         try {
             // Resolve the file path
             Path filePath = fileStorageLocation.resolve(fileName).normalize();
@@ -66,18 +64,16 @@ public class AttachmentController {
                 Tika tika = new Tika();
                 HttpHeaders headers = new HttpHeaders();
                 headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"");
+                headers.add(HttpHeaders.CONTENT_TYPE, tika.detect(resource.getFile().toPath()));
+                headers.add(HttpHeaders.CONTENT_LENGTH, String.valueOf(resource.contentLength()));
 
-                // Read the file and encode it as a Base64 string
-                byte[] fileContent = resource.getInputStream().readAllBytes();
-                String base64Content = Base64.getEncoder().encodeToString(fileContent);
-
-                // Return the Base64 string in the response
-                return new ResponseModel<>(true, "data:" +tika.detect(fileContent)+";" +"base64," + base64Content, "File found");
+                // Read the file and return it
+                return resource.getInputStream().readAllBytes();
             } else {
-                return new ResponseModel<>(false, null, "File not found");
+                throw new ResourceNotFoundException("File not found");
             }
         } catch (MalformedURLException ex) {
-            return new ResponseModel<>(false, null, "File not found");
+            throw new ResourceNotFoundException("File not found");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
