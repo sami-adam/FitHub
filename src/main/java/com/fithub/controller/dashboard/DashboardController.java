b46a177141b9713153476bc799c7f23a9ec58d82
sadam <sami2010mohamed@gmail.com>
2024-09-08 13:20:25 +0300

    Dashboard: Subscriptions

--- src/main/java/com/fithub/controller/dashboard/DashboardController.java
+++ src/main/java/com/fithub/controller/dashboard/DashboardController.java
@@ -11,63 +11,63 @@ import org.springframework.web.bind.annotation.*;
 public class DashboardController {
     private final DashboardService dashboardService;
 
-    @GetMapping(path = "/dashboard/subscriptionsByProduct", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-product", produces = "application/json")
     public String getSubscriptionsByProduct(){
         return dashboardService.getSubscriptionsByProduct().toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByProductCategory", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-product-category", produces = "application/json")
     public String getSubscriptionsByProductCategory(){
         return dashboardService.getSubscriptionsByCategory().toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYear", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year", produces = "application/json")
     public String getSubscriptionsByYear(){
         return dashboardService.getSubscriptionsByYear().toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYear/{year}", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year/{year}", produces = "application/json")
     public String getSubscriptionsByYear(@PathVariable int year){
         return dashboardService.getSubscriptionsByYear(year).toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearAndMonth", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-and-month", produces = "application/json")
     public String getSubscriptionsByYearAndMonth(){
         return dashboardService.getSubscriptionsByYearAndMonth().toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearAndMonth/{year}", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-and-month/{year}", produces = "application/json")
     public String getSubscriptionsByYearAndMonth(@PathVariable int year){
         return dashboardService.getSubscriptionsByYearAndMonth(year).toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearAndMonth/{year}/{month}", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-and-month/{year}/{month}", produces = "application/json")
     public String getSubscriptionsByYearAndMonth(@PathVariable int year, @PathVariable int month){
         return dashboardService.getSubscriptionsByYearAndMonth(year, month).toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearMonthAndDay", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-month-and-day", produces = "application/json")
     public String getSubscriptionsByYearMonthAndDay(){
         return dashboardService.getSubscriptionsByYearMonthAndDay().toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearMonthAndDay/{year}", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-month-and-day/{year}", produces = "application/json")
     public String getSubscriptionsByYearMonthAndDay(@PathVariable int year){
         return dashboardService.getSubscriptionsByYearMonthAndDay(year).toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearMonthAndDay/{year}/{month}", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-month-and-day/{year}/{month}", produces = "application/json")
     public String getSubscriptionsByYearMonthAndDay(@PathVariable int year, @PathVariable int month){
         return dashboardService.getSubscriptionsByYearMonthAndDay(year, month).toString();
     }
 
 
-    @GetMapping(path = "/dashboard/subscriptionsByYearMonthAndDay/{year}/{month}/{day}", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-year-month-and-day/{year}/{month}/{day}", produces = "application/json")
     public String getSubscriptionsByYearMonthAndDay(@PathVariable int year, @PathVariable int month, @PathVariable int day){
         return dashboardService.getSubscriptionsByYearMonthAndDay(year, month, day).toString();
     }
 
-    @GetMapping(path = "/dashboard/subscriptionsByStartDateAndEndDate", produces = "application/json")
+    @GetMapping(path = "/dashboard/subscriptions-by-start-date-and-end-date", produces = "application/json")
     public String getSubscriptionsByStartDateAndEndDate(@RequestParam String startDate, @RequestParam String endDate){
         return dashboardService.getSubscriptionsByStartDateAndEndDate(startDate, endDate).toString();
     }
