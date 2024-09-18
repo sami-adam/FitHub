35123c2812d28e4b71df9619adcc4ea1e2583eca
sadam <sami2010mohamed@gmail.com>
2024-09-18 15:16:16 +0300

    Class Enrollment: Response Entity

--- src/main/java/com/fithub/controller/fitnessClass/ClassEnrollmentController.java
+++ src/main/java/com/fithub/controller/fitnessClass/ClassEnrollmentController.java
@@ -3,6 +3,7 @@ package com.fithub.controller.fitnessClass;
 import com.fithub.dto.fitnessClass.ClassEnrollmentDTO;
 import com.fithub.service.fitnessClass.ClassEnrollmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,23 +16,23 @@ public class ClassEnrollmentController {
     private final ClassEnrollmentService classEnrollmentService;
 
     @GetMapping("/class-enrollments")
-    public List<ClassEnrollmentDTO> getClassEnrollments() {
-        return classEnrollmentService.getClassEnrollments();
+    public ResponseEntity<List<ClassEnrollmentDTO>> getClassEnrollments() {
+        return ResponseEntity.ok(classEnrollmentService.getClassEnrollments());
     }
 
     @GetMapping("/class-enrollment/{id}")
-    public ClassEnrollmentDTO getClassEnrollment(@PathVariable Long id) {
-        return classEnrollmentService.getClassEnrollment(id);
+    public ResponseEntity<ClassEnrollmentDTO> getClassEnrollment(@PathVariable Long id) {
+        return ResponseEntity.ok(classEnrollmentService.getClassEnrollment(id));
     }
 
     @PostMapping("/class-enrollment")
-    public ClassEnrollmentDTO addClassEnrollment(@RequestBody ClassEnrollmentDTO classEnrollmentDTO) {
-        return classEnrollmentService.addClassEnrollment(classEnrollmentDTO);
+    public ResponseEntity<ClassEnrollmentDTO> addClassEnrollment(@RequestBody ClassEnrollmentDTO classEnrollmentDTO) {
+        return ResponseEntity.ok(classEnrollmentService.addClassEnrollment(classEnrollmentDTO));
     }
 
     @PutMapping("/class-enrollment/{id}")
-    public ClassEnrollmentDTO updateClassEnrollment(@PathVariable Long id, @RequestBody ClassEnrollmentDTO classEnrollmentDTO) {
-        return classEnrollmentService.updateClassEnrollment(id, classEnrollmentDTO);
+    public ResponseEntity<ClassEnrollmentDTO> updateClassEnrollment(@PathVariable Long id, @RequestBody ClassEnrollmentDTO classEnrollmentDTO) {
+        return ResponseEntity.ok(classEnrollmentService.updateClassEnrollment(id, classEnrollmentDTO));
     }
 
     @DeleteMapping("/class-enrollment/{id}")
@@ -40,12 +41,12 @@ public class ClassEnrollmentController {
     }
 
     @GetMapping("/class-enrollments/search/{keyword}")
-    public List<ClassEnrollmentDTO> searchClassEnrollments(@PathVariable String keyword) {
-        return classEnrollmentService.searchClassEnrollments(keyword);
+    public ResponseEntity<List<ClassEnrollmentDTO>> searchClassEnrollments(@PathVariable String keyword) {
+        return ResponseEntity.ok(classEnrollmentService.searchClassEnrollments(keyword));
     }
 
     @GetMapping("/class-enrollments/member/{memberId}")
-    public List<ClassEnrollmentDTO> getMemberClassEnrollments(@PathVariable Long memberId) {
-        return classEnrollmentService.getMemberClassEnrollments(memberId);
+    public ResponseEntity<List<ClassEnrollmentDTO>> getMemberClassEnrollments(@PathVariable Long memberId) {
+        return ResponseEntity.ok(classEnrollmentService.getMemberClassEnrollments(memberId));
     }
 }
